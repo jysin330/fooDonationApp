b0526d86b7ab038f826d1f3999062587968a8b5f
Jyoti <jyotisingh82000@gmail.com>
2023-10-17 16:51:46 +0530

    donate model is created

--- mysite/fooDonationApp/models.py
+++ mysite/fooDonationApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Donate(models.Model):
+    donar_id = models.AutoField
+    donarName = models.CharField(max_length=20)
+    phoneNum = models.IntegerField(max_length=14)
+    foodItem = models.CharField(max_length=50)
+    fooDescription = models.CharField(max_length=1000)
+    address = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="fooDonation/images", default="")
+
+    def __str__(self):
+        return self.foodItem
