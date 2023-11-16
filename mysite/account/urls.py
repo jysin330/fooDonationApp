ecad36c5c19a742a46ae538ce6b35a9ce0456505
Jyoti <jyotisingh82000@gmail.com>
2023-11-16 17:43:53 +0530

    logout_view and logout.html created

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
-from .views import login_view
+from django.urls import path
+from .views import login_view, logout_view
 
-urlpatterns = [path("login/", login_view)]
+urlpatterns = [path("login/", login_view), path("logout/", logout_view)]
--- mysite/account/views.py
+++ mysite/account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -17,3 +17,11 @@ def login_view(request):
         return redirect("/home")
 
     return render(request, "fooDonationApp\Accounts\login.html")
+
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("/login")
+
+    return render(request, "fooDonationApp\Accounts\logout.html", {})
