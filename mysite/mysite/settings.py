914b519ccf992b057cb7f1752417664b7c19d0f2
Jyoti <jyotisingh82000@gmail.com>
2024-01-03 14:49:33 +0530

    creating secretKey by  Django Packages

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,6 +20,7 @@ print(BASE_DIR)
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'
 SECRET_KEY =os.environ.get("SECRET_KEY" ,"django-insecure-+t8xz*w-0&d!m5w*xr9t-gp8o8o)-3$acdt)m&@$g+osoe2mlk")
 
 # SECURITY WARNING: don't run with debug turned on in production!
