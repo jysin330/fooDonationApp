1295e1b7175d6c5687d3d0a6ccffc764e792c64d
Jyoti <jyotisingh82000@gmail.com>
2024-01-04 23:03:21 +0530

    Automated test secret key using try execpt

--- mysite/mysite/tests.py
+++ mysite/mysite/tests.py
@@ -1,7 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.password_validation import validate_password
 import os
 class fooDonationAppConfigTest(TestCase):
     def test_secret_key_strength(self):
         # self.assertTrue(1==1)
         SECRET_KEY =os.environ.get("SECRET_KEY")
-        self.assertNotEqual(SECRET_KEY, "abc123")
+        # self.assertNotEqual(SECRET_KEY, "abc123")
+        try:
+            strong_key = validate_password(SECRET_KEY)
+        except Exception as e:
+            msg = f"Weak Password - {e}"
+            self.fail(msg)
