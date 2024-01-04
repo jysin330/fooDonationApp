6b84f2df83cbc1f736e8d62dacc60a7dcb74fe44
Jyoti <jyotisingh82000@gmail.com>
2024-01-04 22:44:43 +0530

    testing secret key strength by giving value not equal to secret key

--- mysite/mysite/tests.py
+++ mysite/mysite/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+import os
 class fooDonationAppConfigTest(TestCase):
-    def test_password(self):
-        self.assertTrue(1==1)
+    def test_secret_key_strength(self):
+        # self.assertTrue(1==1)
+        SECRET_KEY =os.environ.get("SECRET_KEY")
+        self.assertNotEqual(SECRET_KEY, "abc123")
