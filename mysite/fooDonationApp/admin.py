875b385e86bfef59a96d21b010044af338b24cc3
Jyoti <jyotisingh82000@gmail.com>
2024-01-05 13:09:15 +0530

    Listing the donate and recieverUser object by its attribute on the admin page

--- mysite/fooDonationApp/admin.py
+++ mysite/fooDonationApp/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import Donate, ReceiverUser
 
 # Register your models here.
-admin.site.register(Donate)
-admin.site.register(ReceiverUser)
+class DonateAdmin(admin.ModelAdmin):
+    list_display= ['id',"donarName","donarEmail","category"]
+    search_fields= ['id']
+
+class ReceiverUserAdmin(admin.ModelAdmin):
+    list_display= ['id',"receiver_meal","receiver_name","receiver_email"]
+    search_fields= ['id']
+
+admin.site.register(Donate, DonateAdmin)
+admin.site.register(ReceiverUser, ReceiverUserAdmin)
