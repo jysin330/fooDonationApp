1fa4e168ff00f429d036583e927b29da1132a257
Jyoti <jyotisingh82000@gmail.com>
2024-01-08 19:01:43 +0530

    adding new fields in Donate and ReceiverUser model

--- mysite/fooDonationApp/models.py
+++ mysite/fooDonationApp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db import IntegrityError
-
+from django.utils import timezone
 CATEGORY = (
         ("Raw Food", "Raw Food"),
         ("Packed Food", "Packed Food"),
@@ -18,24 +18,25 @@ class Donate(models.Model):
     foodItem = models.CharField(max_length=50, default="")
     fooDescription = models.CharField(max_length=400, default="")
     address = models.CharField(max_length=100, default="")
-
+    timestamp = models.DateTimeField(auto_now_add =True)
+    update = models.DateTimeField(auto_now =True)
+    publish = models.DateField(auto_now_add = False, auto_now =False, default = timezone.now)
     def __str__(self):
         return self.foodItem
 
 
 class ReceiverUser(models.Model):
-    object_meal = Donate.objects.all()
+    # object_meal = Donate.objects.all()
 
-    meal = ()
-    temp = list(meal)
-    for x in object_meal:
-        if x.foodItem:
-            temp.append((f"{x.foodItem}", f"{x.foodItem}"))
-    meal = tuple(temp)
-   
+    # meal = ()
+    # temp = list(meal)
+    # for x in object_meal:
+    #     if x.foodItem:
+    #         temp.append((f"{x.foodItem}", f"{x.foodItem}"))
+    # meal = tuple(temp)
     category = models.CharField(max_length=30, choices=CATEGORY, default="Raw Food")
     receiver_id = models.AutoField
-    receiver_meal = models.CharField(max_length=30, choices=meal, default="")
+    receiver_meal = models.CharField(max_length=30, default="")
     receiver_name = models.CharField(max_length=30)
     receiver_num = models.CharField(max_length=10, default="")
     receiver_email = models.CharField(max_length=50, default="")
@@ -43,4 +44,5 @@ class ReceiverUser(models.Model):
     des = models.TextField()
     timestamp = models.DateTimeField(auto_now_add =True)
     update = models.DateTimeField(auto_now =True)
+    publish = models.DateField(auto_now_add = False, auto_now =False, default = timezone.now)
 
