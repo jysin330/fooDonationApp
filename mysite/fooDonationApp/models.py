5e728ded0a54568521a39ffa17942c21b1da6c74
Jyoti <jyotisingh82000@gmail.com>
2024-01-21 18:15:36 +0530

    testing querySet exists or not

--- mysite/fooDonationApp/models.py
+++ mysite/fooDonationApp/models.py
@@ -37,24 +37,6 @@ class Donate(models.Model):
         super().save(*args, **kwargs)
         # obj.save()
         # do another something
-
-def slugify_instance_foodItem(instance, save=False,new_slug =None):
-    if new_slug is not None:
-        slug= new_slug
-    else:
-        slug = slugify(instance.foodItem)
-
-    Klass = instance.__class__
-    qs = Klass.objects.filter(slug=slug).exclude(id = instance.id)
-    if qs.exists():
-        rand_int = random.randint(300_000,500_000)
-        slug = f"{slug}-{rand_int}"
-        return slugify_instance_foodItem(instance, save=save, new_slug= slug)
-    instance.slug = slug
-    if save:
-        instance.save()
-    return instance
-
      
 
 #  instance is the actual instance of whatever the model that's being sent and sender is the actual model class itself which we can test out by doing sender and instance.
--- mysite/fooDonationApp/tests.py
+++ mysite/fooDonationApp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Donate
+
+class DonateTestCase(TestCase):
+    def setUp(self):
+        Donate.objects.create(donarName= "jyoti singh", foodItem= "vegies" ,address= "bhaluhi")
+        
+
+    def test_queryset_exists(self):
+        qs = Donate.objects.all()
+        self.assertTrue(qs.exists())
